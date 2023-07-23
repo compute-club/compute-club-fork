8652d54e87c90a6a649b7fcf89e55d9b2f360e87
RRustom <ramirustom@gmail.com>
2023-07-23 18:33:26 +0000

    remove lmgym_train code that was used for docker setup

--- chai_research/lmgym_train/train/explorations/lmgym/reward_models/config.py
+++
@@ -1,5 +0,0 @@
-import os
-
-
-HF_TOKEN = os.environ.get('HUGGINGFACE_TOKEN')
-WANDB_AUTH_TOKEN = os.environ.get('WANDB_AUTH_TOKEN')
--- chai_research/lmgym_train/train/explorations/lmgym/reward_models/experiments/v0_retry_accept/run.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-
-export DATA=12000000
-python -m torch.distributed.launch --nproc_per_node=4 train.py
